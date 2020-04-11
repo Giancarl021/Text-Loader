b4c2643bd18f2ac2e7cc76cf28a9c7b7fe78ad5b
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-04-10 23:17:01 -0300

    nested mark done

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -2,6 +2,19 @@ function init() {
     const span = document.querySelector('span');
     span.innerHTML = html;
     span.setAttribute('contenteditable', isEditable);
+
+    if (isEditable) {
+        span.onfocus = function () {
+            this.innerHTML = this.innerHTML
+                .replace(/<mark>/g, '{')
+                .replace(/<\/mark>/g, '}');
+        }
+        span.onblur = function () {
+            this.innerHTML = this.innerHTML
+                .replace(/{/g, '<mark>')
+                .replace(/}/g, '</mark>');
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', init);
